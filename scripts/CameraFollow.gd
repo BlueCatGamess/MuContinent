1d4fa2342b509d56427e8fa1f8133dc91d492585
BlueCatGamess <63529201+PHandradee@users.noreply.github.com>
2023-12-16 18:53:20 -0300

    Creation of Ground Ray Detector

--- scripts/CameraFollow.gd
+++ scripts/CameraFollow.gd
@@ -6,6 +6,8 @@ extends Component
 @export var offset: Vector3 = Vector3.ZERO;
 @export var smooth_force: float = 1.0;
 
+var desired_position: Vector3 = Vector3.ZERO;
+var smoothed_position: Vector3 = Vector3.ZERO;
 
 func _ready() -> void:
 	#FIXME Ebus.playerEntered.connect(OnPlayerEntered);
@@ -17,9 +19,13 @@ func _physics_process(_delta) -> void:
 	if target == null:
 		return
 	
-	var desired_position: Vector3 = target.global_transform.origin + offset;
-	var smoothed_position: Vector3 = self.main_actor.global_transform.origin.lerp(desired_position, smooth_force)
+	if smoothed_position == self.main_actor.global_transform.origin:
+		return
+
+	desired_position = target.global_transform.origin + offset;
+	smoothed_position = self.main_actor.global_transform.origin.lerp(desired_position, smooth_force)
 	self.main_actor.global_transform.origin = smoothed_position;
 
 func OnPlayerEntered(player: CharacterBody3D) -> void:
 	target = player;
+
---
+++ scripts/EventBus.gd
@@ -0,0 +1,4 @@
+extends Node
+
+
+signal GroundLeftClicked(position: Vector3);
